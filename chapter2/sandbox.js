306ee302fd4598eb90dc808c6fc7f8fe3f287c67
GSarpong4423 <sarpongg@gmail.com>
2020-11-02 12:27:14 -0500

    variables, constants and comment

--- chapter2/sandbox.js
+++ chapter2/sandbox.js
@@ -1,1 +1,20 @@
-alert('Hello, World');
+//variable, constants, comments
+
+
+// 'let' which is interchangable variable
+let age = 25;
+let year = 2020;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+
+//Conststants 'const' which is NOT interchangable variable
+const points = 100
+console.log(points);
+
+//older way to create variable
+var score = 75;
+console.log(score);
